5ae27972f145924bd4cb86bd3e761382b3efba72
Diego Reyes <diegoreyes@diegoworks.com>
2026-03-20 15:53:06 +0100

    test: implement automated API testing scenario using node:test and fastify inject

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,13 @@
-import Fastify from 'fastify';
-import { productRoutes } from './routes/product.routes.js';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-const fastify = Fastify({
-    logger: true,
-});
-
-fastify.register(productRoutes, { prefix: '/api' });
+import { app } from './app.js';
 
 const start = async () => {
     try {
         const PORT = Number(process.env.PORT) || 4000;
 
-        await fastify.listen({ port: PORT, host: '0.0.0.0' });
+        await app.listen({ port: PORT, host: '0.0.0.0' });
         console.log(`Server running at http://localhost:${PORT}`);
     } catch (err) {
-        fastify.log.error(err);
+        app.log.error(err);
         process.exit(1);
     }
 };
